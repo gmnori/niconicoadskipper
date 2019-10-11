25fc4f87dbc398933414b693e497569e8d71ee31
gmnori <gmnoir@yahoo.co.jp>
2019-10-11 22:51:02 +0900

    js adjustment

--- nicoadskipper.js
+++ nicoadskipper.js
@@ -1,16 +1,11 @@
 // JavaScript source code
-
 adSkip = setInterval(skip, 100);
-var s = 0;
 
 function skip() {
     const elem1 = document.getElementsByTagName('video');
         if (elem1[1] == null){
         } else if(elem1[1].src !=""){
             elem1[1].src="";
-            s++;
-        }
-        if(s > 3){
             clearInterval(adSkip);
         }
 }
